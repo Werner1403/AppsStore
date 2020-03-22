6e675080eddcc2b0ee2e93cb8c2f959e83f331fe
Spinn <Spinn@LAPTOP-FTT3OF6P>
2020-03-22 20:30:59 +0100

    application mit main screen

---
+++ AppStore/src/application/ConfirmBox.java
@@ -0,0 +1,63 @@
+
+package application;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ConfirmBox {
+	
+	static boolean answer;
+	
+	public static boolean display(String title,String message) {
+		
+		Stage window = new Stage();
+		
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		window.setMinWidth(250);		
+		Label label = new Label();
+		label.setText(message);
+		
+		//create 2 buttons
+		Button yesButton= new Button("yes");		
+		Button noButton = new Button("no");
+		
+		yesButton.setOnAction(e -> {
+			answer = true;
+			window.close();
+		});
+		
+		noButton.setOnAction(e -> {
+			answer = false;
+			window.close();
+		});
+		
+		
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(label,yesButton,noButton);
+		layout.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(layout);
+		window.setScene(scene);
+		scene.getStylesheets().add(Main_StartView.class.getResource("TheStyle.css").toExternalForm());
+		window.setResizable(false);
+		window.showAndWait();
+		
+		return answer;
+		
+	}
+	
+}
---
+++ AppStore/src/application/Want_to_close.java
@@ -0,0 +1,61 @@
+package application;
+
+
+
+import java.awt.Window;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Want_to_close extends Application{
+
+	Button button;
+	Stage window;
+	
+	
+	public static void main(String[] args) {
+	
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		window = primaryStage;
+		
+		window.setOnCloseRequest(e ->{
+			e.consume();
+			closeProgramm();
+			});
+		
+		button = new Button();
+		button.setText("close");			
+		button.setOnAction(e ->{closeProgramm();});
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Parent root;
+		Scene scene = new Scene(layout,300,500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	private void closeProgramm() {
+		Boolean answer = ConfirmBox.display("Close?", "You want to close?");
+		if(answer) {
+			window.close();
+		}
+		
+		
+	}
+	
+
+}
